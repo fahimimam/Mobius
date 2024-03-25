7abf3b38510091aeab143dac2b1f6ac5cf98f277
Kazi Fahim Imam <fahimimam@iut-dhaka.edu>
2024-03-25 12:45:30 +0600

    Working css with adding this one in the heading or inline css

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		render(w, "test.page.gohtml")
-	})
+	staticFileDirectory := http.Dir("/assets/css/")
+	log.Println("Got Directory as ", staticFileDirectory)
+	staticFileHandler := http.FileServer(staticFileDirectory) //http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	http.Handle("/assets/css/", staticFileHandler)
 
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		render(w, "register.page.gohtml")
-	})
+	http.HandleFunc("/home", landingPage)
+	http.HandleFunc("/register", register)
+
 	fmt.Println("Starting front end service on port 3000")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
@@ -22,11 +23,38 @@ func main() {
 	}
 }
 
+func landingPage(w http.ResponseWriter, r *http.Request) {
+	render(w, "test.page.gohtml")
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", "register.page.gohtml"))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	log.Println("Executed Templates")
+}
+
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
 		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
 		"./cmd/web/templates/footer.partial.gohtml",
 	}
 
